d22b522cb0896169b66b322e76fbccd87d22bd83
Daniel Pacak <1322923+danielpacak@users.noreply.github.com>
2025-09-16 09:59:32 +0200

    Update Hello Flask 3.10

--- examples/python/hello-flask-python3.10/hack/import-image-to-containerd.sh
+++ examples/python/hello-flask-python3.10/hack/import-image-to-containerd.sh
@@ -1,4 +1,9 @@
-docker buildx build --load -t python-hello-flask -f Dockerfile .
+IMAGE_REF="docker.io/danielpacak/python-hello-flask:python3.10-glibc"
+IMAGE_TAR="/tmp/python-hello-flask.tar"
 
-docker image save -o /tmp/python-hello-flask.tar python-hello-flask:latest
-sudo ctr -n k8s.io image import /tmp/python-hello-flask.tar && rm /tmp/python-hello-flask.tar
+# TODO Check if the ctr is installed!
+
+docker buildx build --load -t $IMAGE_REF -f Dockerfile .
+
+docker image save -o $IMAGE_TAR $IMAGE_REF
+sudo ctr -n k8s.io image import $IMAGE_TAR && rm $IMAGE_TAR
